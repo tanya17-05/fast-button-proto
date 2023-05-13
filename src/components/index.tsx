8b4fee011c6cc1b4bd074aadb4ad7488aa4a2bd6
Tanya <tanyakumari332@gmail.com>
2023-05-13 12:10:45 +0530

    refractor: customizations

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,1 +1,7 @@
+import { DesignSystem } from "@microsoft/fast-foundation";
+
 export * from "./ls-login-anchor"
+
+export function provideGA4GHSystem(element?: HTMLElement): DesignSystem {
+    return DesignSystem.getOrCreate(element).withPrefix("ga4gh");
+}
--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,46 @@
 import {
   fastAnchor,
-  provideFASTDesignSystem,
   controlCornerRadius,
   neutralPalette,
   PaletteRGB,
   baseLayerLuminance,
   StandardLuminance,
   SwatchRGB,
+  density,
+  typeRampBaseFontSize,
+  typeRampMinus1FontSize
 } from "@microsoft/fast-components";
-import { lsLoginAnchor } from "./components";
+import { lsLoginAnchor, provideGA4GHSystem } from "./components";
 import { parseColorHexRGB } from "@microsoft/fast-colors";
 
-provideFASTDesignSystem().register(
-  fastAnchor(),
+// registering web-component
+provideGA4GHSystem().register(
+  fastAnchor(), // important to use default design tokens
   lsLoginAnchor(),
 );
 
+const firstAnchor = document.querySelector("ga4gh-ls-login-anchor") as HTMLElement;
+
+// changing default border radius
 controlCornerRadius.withDefault(0);
 
+// switching to ligh mode
 baseLayerLuminance.withDefault(StandardLuminance.LightMode);
 
-const firstAnchor = document.querySelector("fast-ls-login-anchor") as HTMLElement;
 
+// creating base color
 neutralPalette.withDefault(
   PaletteRGB.create(SwatchRGB.from(parseColorHexRGB("#00ff00")!))
 );
 
+// changing color for specific element
 neutralPalette.setValueFor(
   firstAnchor,
   PaletteRGB.create(SwatchRGB.create(0.1, 0, 1))
 );
+
+// changing the font size
+typeRampBaseFontSize.withDefault(typeRampMinus1FontSize)
+
+// changing the padding
+density.withDefault(2)
